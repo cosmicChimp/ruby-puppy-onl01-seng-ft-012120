44b07bca3f6c4fd5d15d224c09734631bfd33b74
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-01 09:51:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,13 +14,16 @@ class Dog
   def self.all
     @@all.each do |dog|
       puts dog.name
-    end
   end
   
   def self.clear_all
     @@all.clear 
   end
 
+  def self.print_all
+    @@all
+    end
+  end
 
 end
 
